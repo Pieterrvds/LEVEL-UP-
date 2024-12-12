89f6ac76f2094878d226546e7b2b3ebc699f86b7
Pieterrvds <PieterV-D-S@hotmail.com>
2024-12-12 01:33:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,23 +11,7 @@ headerImages.forEach(image => {
     image.style.transform = 'scale(1)';
   });
 });
-// Select all program elements and the image container
-const programs = document.querySelectorAll('.program');
-const programImageContainer = document.getElementById('program-image-container');
-const programImage = document.getElementById('program-image');
 
-// Add click event listeners to each program
-programs.forEach(program => {
-  program.addEventListener('click', () => {
-    // Get the image URL from the data-image attribute
-    const imageUrl = program.getAttribute('src');
-    
-    // Set the image src and display the image container
-    programImage.src = imageUrl;
-    programImage.alt = program.querySelector('h3').textContent;
-    programImageContainer.style.display = 'block';
-  });
-});
 // Get the Read More button and the extra text
 const readMoreButton = document.getElementById('read-more-button');
 const extraText = document.getElementById('extra-text');
@@ -43,3 +27,32 @@ readMoreButton.addEventListener('click', () => {
     readMoreButton.textContent = 'Read More'; // Change button text back
   }
 });
+
+// Select all program cards
+const programCards = document.querySelectorAll('.program-card');
+
+    programCards.forEach(card => {
+    // Add click event listener to toggle the "flipped" class
+    card.addEventListener('click', () => {
+      card.classList.toggle('flipped');
+    });
+  });
+
+  // Select all team members
+  const teamMembers = document.querySelectorAll('.team-member');
+
+  // Add a double-click event to each team member
+  teamMembers.forEach(member => {
+    member.addEventListener('dblclick', () => {
+      const url = member.getAttribute('data-url'); // Get the URL from the data attribute
+      if (url) {
+        window.open(url, '_blank'); // Open the URL in a new tab
+      }
+      if (!url) {
+        alert('Link to this trainer comming soon ;)');
+      }
+
+    });
+  });
+
+
